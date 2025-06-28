d6080f98e4fbc71bc079d0736552716ccf56da04
fahrir <fakhrirrahman7@gmail.com>
2025-06-28 14:03:23 +0700

    feat: add getRedirectUrl method to CreateCity, CreateInfographic, and CreateShip classes for improved resource redirection

--- app/Filament/Resources/InfographicResource/Pages/CreateInfographic.php
+++ app/Filament/Resources/InfographicResource/Pages/CreateInfographic.php
@@ -9,4 +9,8 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateInfographic extends CreateRecord
 {
     protected static string $resource = InfographicResource::class;
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl();
+    }
 }
--- app/Filament/Resources/ShipResource/Pages/CreateShip.php
+++ app/Filament/Resources/ShipResource/Pages/CreateShip.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateShip extends CreateRecord
 {
     protected static string $resource = ShipResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl();
+    }
 }
